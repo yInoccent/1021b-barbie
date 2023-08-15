08c3990ad94532509d5103cbb152eef0748dc9c6
RELVIN TEIXEIRA BERTULINO <08965187109@ifms.edu.br>
2023-08-15 07:34:14 -0400

    primeiro commit

---
+++ src/componentes/header/Header.tsx
@@ -0,0 +1,20 @@
+import './Header.css'
+
+function Header(){
+    return(
+        <header>
+            <div>
+                logo
+            </div>
+            <nav>
+                <ul>
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">Sobre</a></li>
+                    <li><a href="#">Notícias</a></li>
+                    <li><a href="#">contato</a></li>
+                </ul>
+            </nav>
+        </header>
+    )
+}
+export default Header
